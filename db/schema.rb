471864af2598ee1e0a9989c6557d01343a14f96d
nonress <b.nxxxi551@gmail.com>
2019-09-12 05:09:46 +0900

    edit users_controller#create

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_103858) do
+ActiveRecord::Schema.define(version: 2019_09_11_071332) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "coname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.string "coname"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
