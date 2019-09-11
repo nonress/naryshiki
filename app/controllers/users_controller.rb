70cbbb4141ad5646d0e4a2e0fcaa88b76dd21605
nonress <b.nxxxi551@gmail.com>
2019-09-12 04:47:28 +0900

    could change users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:succes] = 'ようこそ、なりしきの世界へ'
+      redirect_to @user
+      # => "/users/#{@user.id}"
+      # user_path(@user
+      # user_path(@user.id)
     else
+      #flash[:danga]
       render 'new'
     end
   end
