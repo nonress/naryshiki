3d9175aea8cc9ce0e773d76f6ef7e9438c9faf70
nonress <b.nxxxi551@gmail.com>
2019-09-12 18:11:09 +0900

    change class users-show => form-template

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
-    asset_no_difference 'User.count' do
-      posr users_path, params: { user: { name: '',
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
                         email: 'user@invalid',
                      password: 'foo',
          passord_confirmation: 'bar'}}
     end
-    asser_template 'users/new'
+    assert_template 'users/new'
   end
 end
