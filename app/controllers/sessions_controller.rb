66ea8f3045317e2b70fc45c2ac4ef73bc827beb5
nonress <b.nxxxi551@gmail.com>
2019-09-12 17:14:59 +0900

    edit header-padding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
+    @user = User.find_by(email: params[:sessionss][:email])
+    if @user.autenticate(params[:sessionss][:password])
+
+    else
+      flash[:danger] = 'メールアドレスもしくはパスワードが間違っています'
+      render 'new'
+    end
   end
 end
