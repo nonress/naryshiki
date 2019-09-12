0a2a8a4a589bc0a101670e912814a31b12cf3cec
nonress <b.nxxxi551@gmail.com>
2019-09-13 01:06:31 +0900

    finish basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'ようこそ、なりしきの世界へ'
       redirect_to @user
       # => "/users/#{@user.id}"
