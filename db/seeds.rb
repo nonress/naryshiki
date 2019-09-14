614c88089020c85edf6ba843c601f2d3b0e9da56
nonress <b.nxxxi551@gmail.com>
2019-09-14 21:05:52 +0900

    gem install will_paginate && bootstrap-will_paginate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             coname: 'example.company',
+             password:              "foobar",
+             password_confirmation: "foobar")
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  coname = "example.company#{n+1}"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               coname: coname,
+               password:              password,
+               password_confirmation: password)
+end
