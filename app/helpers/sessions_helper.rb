0aef6c77757a6e065cac16f9664dee403aa76e69
nonress <b.nxxxi551@gmail.com>
2019-09-14 15:52:04 +0900

    make check box login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,20 +3,36 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def remember(user)
+    user.remembers
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-    #if @current_user
-    #  @current_user = User.find_by(id: session[:user_id])
-    #else
-    #  @current_user
-    #end
+    if user_id = session[:user_id]
+      @current_user ||= User.find_by(id: user_id)
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forgets
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
