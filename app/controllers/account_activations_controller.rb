aa94f51c54ddda2e1fa2aa3fa1551eb705356c43
nonress <b.nxxxi551@gmail.com>
2019-09-16 05:03:14 +0900

    account action && sendgrid

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,14 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = "アカウント有効かできました"
+      redirect_to user
+    else
+      flash[:danger] = "既にアカウントは有効化済みです"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] =='1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "アカウント有効化が未完了です。メールを確認しアカウント有効化を行なってください。"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'メールアドレスもしくはパスワードが間違っています'
       render 'new'
