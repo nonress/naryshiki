0ab6a23ee4aa93fd0ba2394d3a2dedd146d53823
nonress <b.nxxxi551@gmail.com>
2019-09-15 20:15:33 +0900

    rails g migtation add_activation_to_users activation_digest:string activated:boolean activated_at:datetime

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
   root 'basic_pages#home'
   get '/signup', to: 'users#new'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
