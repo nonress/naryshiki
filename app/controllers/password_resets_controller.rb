5689bfe90cb0151feb0d6f085b64eaad65effdb7
nonress <b.nxxxi551@gmail.com>
2019-09-16 19:42:49 +0900

    edit password contorller create

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -26,7 +26,7 @@ class PasswordResetsController < ApplicationController
     if params[:user][:password].empty?
       @user.errors.add(:password, :blank)
       render 'edit'
-    elsif @user.update_attribute(user_params)
+    elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "パスワードを更新しました"
       redirect_to @user
