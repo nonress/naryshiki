7272140b47cbe3ef438ff05e855e7cb8b69a87f0
nonress <b.nxxxi551@gmail.com>
2019-09-17 22:45:09 +0900

    edit users/contoller followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,8 +57,8 @@ class UsersController < ApplicationController
 
   def followers
     @title = "Followers"
-    @user = @user.followers.paginate(page: params[:page])
-    @users = @user.followets.paginate(page: params[:page])
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
 
