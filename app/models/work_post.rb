0de5250f4a87225a3d4d5c234f5c30fda332f583
nonress <b.nxxxi551@gmail.com>
2019-10-05 21:15:06 +0900

    validates :category_ids

--- app/models/work_post.rb
+++ app/models/work_post.rb
@@ -2,6 +2,8 @@ class WorkPost < ApplicationRecord
   belongs_to :user
   has_rich_text :body
   default_scope -> { order(created_at: :desc)}
-  has_many :work_post_category_relations
+  has_many :work_post_category_relations, dependent: :destroy
   has_many :categories, through: :work_post_category_relations
+  validates :category_ids, presence: true
+
 end
