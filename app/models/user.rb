3fc1b30b2c91597612ef576b18d2e13be23fd345
nonress <b.nxxxi551@gmail.com>
2019-11-19 21:54:46 +0900

    modification app/models/user.rb for feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,8 +68,8 @@ class User < ApplicationRecord
   end
 
   def feed
-    following_ids = "SELECT followed_id FROM relationships
-                     WHERE follower_id = :user_id"
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})OR user_id = :user_id", user_id: id)
     Micropost.microposts_scope
   end
 
