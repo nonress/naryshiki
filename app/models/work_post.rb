bd5ddf5371080e56a9a51a13a31d3a1656c1fa42
nonress <b.nxxxi551@gmail.com>
2019-11-19 21:49:21 +0900

    Change default_scope to work_posts_scope for WorkPost

--- app/models/work_post.rb
+++ app/models/work_post.rb
@@ -1,9 +1,8 @@
 class WorkPost < ApplicationRecord
   belongs_to :user
   has_rich_text :body
-  default_scope -> { order(created_at: :desc)}
+  scope :work_posts_scope, -> { order(created_at: :desc)}
   has_many :work_post_category_relations, dependent: :destroy
   has_many :categories, through: :work_post_category_relations
   validates :categories, presence: true
-
 end
