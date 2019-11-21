af7e6d89d57e12bd18960a7516eb2fa790f5de86
nonress <b.nxxxi551@gmail.com>
2019-11-21 18:37:14 +0900

    Change method UserClassMethod -> self

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,13 +19,13 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 },allow_nil: true
   validates :coname,   presence: true, length: { maximum: 20 }
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
